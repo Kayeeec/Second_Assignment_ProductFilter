9cb0dca76e19190815e266d2b5ba100f8042e7e0
karolina.bozkova <karolina.bozkova@artin.cz>
2019-04-17 18:42:09 +0200

    First test.

---
+++ src/test/java/cz/muni/fi/pv260/productfilter/AtLeastNOfFilterTest.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pv260.productfilter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AtLeastNOfFilterTest {
+
+    @org.junit.jupiter.api.Test
+    void passes() {
+    }
+}
